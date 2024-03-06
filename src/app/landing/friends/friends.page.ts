27da699cac1a2f500155feab1d1efac419025270
radsylph <andresa@devtop.io>
2024-03-06 03:12:17 -0400

    chore: update friends components

--- src/app/landing/friends/friends.page.ts
+++ src/app/landing/friends/friends.page.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.page.html',
   styleUrls: ['./friends.page.scss'],
 })
 export class FriendsPage implements OnInit {
-
-  constructor() { }
+   items:any = [];
 
   ngOnInit() {
+    this.generateItems();
   }
 
+  private generateItems() {
+    const count = this.items.length + 1;
+    for (let i = 0; i < 50; i++) {
+      this.items.push(`Item ${count + i}`);
+    }
+  }
+
+  onIonInfinite(ev:any) {
+    this.generateItems();
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
 }
