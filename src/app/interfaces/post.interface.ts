4db22783825906349ffef3aea1f1f246063891a9
radsylph <andresa@devtop.io>
2024-03-06 16:08:19 -0400

    Chore: added new fields to the posts/publications interfaces

---
+++ src/app/interfaces/post.interface.ts
@@ -0,0 +1,23 @@
+export interface PostInterface {
+    publications: PostInterface[];
+    _id: string;
+    owner: {
+        email: string;
+        lastname: string;
+        name: string;
+        password: string;
+        username: string;
+        _id: string;
+    };
+    title: string;
+    content: string;
+    images: [string];
+    isEdited: boolean;
+    bookMark: boolean;
+    likes: number;
+    comments: number;
+    createdAt: string;
+    updatedAt: string;
+    Isliked: boolean;
+
+}
