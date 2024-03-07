31552dee89b97b7a84a7d6fb5c300aae01b48ada
radsylph <andresa@devtop.io>
2024-03-07 16:21:43 -0400

    etst

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -114,9 +114,27 @@ export class UserServices {
             return this.http.patch(`${this.BackenUrl}/publications/like/${postId}`, {}, {
                 headers: { Authorization: `Bearer ${token}` },
             }).toPromise().then((res: any) => {
+
                 return res;
             }
             )
+        } catch (error) {
+            return error;
+        }
+    }
+
+    getLikedPosts(token: any) {
+        if (token === "" || token === undefined || token === null) {
+            console.log("no hay token");
+            return;
+        }
+        try {
+            return this.http.get(`${this.BackenUrl}/publications/like`, {
+                headers: { Authorization: `Bearer ${token}` },
+            }).toPromise().then((res: any) => {
+                console.log(res);
+                return res.likes;
+            })
         } catch (error) {
             return error;
         }
