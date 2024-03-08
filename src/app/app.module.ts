34f06ccd2cc6ab0a9363739a91f0c11b553b5d09
radsylph <andresa@devtop.io>
2024-03-08 11:22:19 -0400

    Chore:added ionic camera and initialize firebase project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, CommonModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, CommonModule, HttpClientModule, provideFirebaseApp(() => initializeApp({ "projectId": "junktion-effaa", "appId": "1:875702451775:web:5f9ca39d880593e3d7428a", "storageBucket": "junktion-effaa.appspot.com", "apiKey": "YOUR_API_KEY", "authDomain": "junktion-effaa.firebaseapp.com", "messagingSenderId": "875702451775", "measurementId": "G-WE2B7VCZZF" })), provideStorage(() => getStorage())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, UserServices],
   bootstrap: [AppComponent],
 })
