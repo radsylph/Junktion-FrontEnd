408a2ac91a346d69673cdee30be47c3a71b2c588
radsylph <andresa@devtop.io>
2024-03-13 02:15:08 -0400

    Chore:added swiper

--- src/app/landing/details/details.module.ts
+++ src/app/landing/details/details.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -7,6 +7,9 @@ import { IonicModule } from '@ionic/angular';
 import { DetailsPageRoutingModule } from './details-routing.module';
 
 import { DetailsPage } from './details.page';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @NgModule({
   imports: [
@@ -15,6 +18,7 @@ import { DetailsPage } from './details.page';
     IonicModule,
     DetailsPageRoutingModule
   ],
-  declarations: [DetailsPage]
+  declarations: [DetailsPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class DetailsPageModule {}
+export class DetailsPageModule { }
