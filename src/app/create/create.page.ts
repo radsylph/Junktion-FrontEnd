77c6ed0de118cfd0f723ca3d3b12b312ec9e8453
radsylph <andresa@devtop.io>
2024-03-13 02:11:53 -0400

    Chore:added validation for when the image is too big

--- src/app/create/create.page.ts
+++ src/app/create/create.page.ts
@@ -112,6 +112,18 @@ export class CreatePage implements OnInit {
       bstr = atob(arr[1]),
       n = bstr.length,
       u8arr = new Uint8Array(n);
+
+
+    const sizeInMB = n / (1024 * 1024);
+    if (sizeInMB > 3) {
+      const alert = this.alert.create({
+        header: 'Error',
+        message: 'The image is too big',
+        buttons: ['OK']
+      }).then(alert => alert.present());
+      return;
+    }
+
     while (n--) {
       u8arr[n] = bstr.charCodeAt(n);
     }
